95bd459e1bf74ce1b3952b09e59cf770ec542df2
Kenny <Kenny.Naidoo@psg.co.za>
2025-12-17 19:33:45 +0200

    Added logs to login controller

--- FirstAPI/Controllers/LoginController.cs
+++ FirstAPI/Controllers/LoginController.cs
@@ -9,13 +9,14 @@ namespace FirstAPI.Controllers
     public class LoginController : ControllerBase
     {
         private ILoginService _loginService;
+        private ILogger<LoginController> _logger;
 
 
-        public LoginController(ILoginService loginService) { 
-        
-               _loginService = loginService;
-        
-        
+        public LoginController(ILoginService loginService, ILogger<LoginController> logger)
+        {
+
+            _loginService = loginService;
+            _logger = logger;
         }
 
 
@@ -24,14 +25,17 @@ namespace FirstAPI.Controllers
 
 
             try {
+                _logger.LogInformation($"Entered Authenticate Controller: username = {username} and password = {password}");
 
                 return await _loginService.Authenticate(username, password);
-
+                 
 
 
 
             }
             catch (Exception ex) {
+
+                _logger.LogError($"Error in Authenticate Controller: {ex.Message}  {ex.InnerException}");
 
 
                 throw ex;
